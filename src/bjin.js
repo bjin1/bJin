5d0514a6843e1bcaeb52e4623832094f6887d5c2
bjin1 <neo_jin_kmyz@yahoo.com>
2021-01-28 01:11:22 +0800

    make furthur delete for useless files

--- src/bjin.js
+++ src/bjin.js
@@ -14,7 +14,10 @@ Syntax: src/bjin.js <filename> check
 async function compileFromFile(filename, outputType) {
   try {
     const buffer = await fs.readFile(filename)
-    console.log(compile(buffer.toString(), outputType))
+    if (compile(buffer.toString(), outputType))
+    {
+      console.log("Syntax has no problem.")
+    }
   } catch (e) {
     console.error(`${e}`)
     process.exitCode = 1
--- src/parser.js
+++ src/parser.js
@@ -4,9 +4,8 @@
 // as a string and returns the AST.
 
 import ohm from "ohm-js"
-import * as ast from "./ast.js"
 
-const aelGrammar = ohm.grammar(String.raw`Ael {
+const aelGrammar = ohm.grammar(String.raw`bJin {
   Program   = Statement+
   Statement = let id "=" Exp                  --variable
             | id "=" Exp                      --assign
@@ -29,46 +28,10 @@ const aelGrammar = ohm.grammar(String.raw`Ael {
   space    += "//" (~"\n" any)* ("\n" | end)  --comment
 }`)
 
-const astBuilder = aelGrammar.createSemantics().addOperation("ast", {
-  Program(body) {
-    return new ast.Program(body.ast())
-  },
-  Statement_variable(_let, id, _eq, expression) {
-    return new ast.Variable(id.sourceString, expression.ast())
-  },
-  Statement_assign(id, _eq, expression) {
-    return new ast.Assignment(
-      new ast.IdentifierExpression(id.sourceString),
-      expression.ast()
-    )
-  },
-  Statement_print(_print, expression) {
-    return new ast.PrintStatement(expression.ast())
-  },
-  Exp_binary(left, op, right) {
-    return new ast.BinaryExpression(op.sourceString, left.ast(), right.ast())
-  },
-  Term_binary(left, op, right) {
-    return new ast.BinaryExpression(op.sourceString, left.ast(), right.ast())
-  },
-  Factor_unary(op, operand) {
-    return new ast.UnaryExpression(op.sourceString, operand.ast())
-  },
-  Factor_parens(_open, expression, _close) {
-    return expression.ast()
-  },
-  num(_whole, _point, _fraction) {
-    return new ast.Literal(Number(this.sourceString))
-  },
-  id(_first, _rest) {
-    return new ast.IdentifierExpression(this.sourceString)
-  },
-})
-
 export default function parse(sourceCode) {
   const match = aelGrammar.match(sourceCode)
   if (!match.succeeded()) {
     throw new Error(match.message)
   }
-  return astBuilder(match).ast()
+  return true 
 }
